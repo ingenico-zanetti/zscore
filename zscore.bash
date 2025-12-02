8a7c0b6fdba49e799293d77851ad568c270005fd
Arnaud ZANETTI <arnaud.zanetti@free.fr>
2025-12-02 18:44:27 +0100

    Add an example of invocating zscore2

---
+++ zscore.bash
@@ -0,0 +1,10 @@
+#!/bin/bash
+# DPFR020 14/12/2025
+# ./zscore2 --address 0.0.0.0 --bind 0.0.0.0 -h 9090 -s 8366 --local "USCP Portes 1" --visitor "SL Margès 2" 2>&1 | tee -a zscore.log
+# DPFR017 06/12/2025
+./zscore2 --address 0.0.0.0 --bind 0.0.0.0 -h 9090 -s 8366 --local "2GVB 3" --visitor "USCP Portes 1" 2>&1 | tee -a zscore.log
+# PRCR011 23/11/2025
+# ./zscore2 --address 0.0.0.0 --bind 0.0.0.0 -h 9090 -s 8366 --local "USCP Portes 1" --visitor "Valence ACE 3" 2>&1 | tee -a zscore.log
+# PRCA003 21/11/2025
+# ./zscore2 --address 0.0.0.0 --bind 0.0.0.0 -h 9090 -s 8366 --local "USCP Portes 1" --visitor "Montélimar VB 2" 2>&1 | tee -a zscore.log
+
