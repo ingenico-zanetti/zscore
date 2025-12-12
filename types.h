be52a0bf13a1d477676c3cdd304c4dcf9c1d27ab
Arnaud ZANETTI <arnaud.zanetti@free.fr>
2025-12-12 22:57:54 +0100

    Define pixel_s in a common file

---
+++ types.h
@@ -0,0 +1,12 @@
+#ifndef __TYPE_H_INCLUDED__
+#define __TYPE_H_INCLUDED__
+
+typedef struct {
+	unsigned char red;
+	unsigned char green;
+	unsigned char blue;
+	unsigned char alpha;
+} pixel_s;
+
+#endif // __TYPE_H_INCLUDED__
+       //
